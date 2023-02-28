f475e30949ebe073253480712080ab5a58e56554
HisyamWildan <hisyamwildan79@gmail.com>
2023-02-28 15:18:22 +0700

    Upload Praktik2

---
+++ Praktikum2/form_hasil.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hasil Nilai Mahasiswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+<div class="container">
+    <table class="table text-uppercase table-bordered">
+        <tr class="table-primary">
+            <th>Nama</th>
+            <th>Mata Kuliah</th>
+            <th>Tugas</th>
+            <th>UTS</th>
+            <th>UAS</th>
+            <th>Total Nilai</th>
+            <th>keterangan</th>
+        </tr>
+        <?php
+        require_once "proses_nilai.php";
+        ?>
+        <tr>
+            <td><?= $nama;?></td>
+            <td><?= $matkul;?></td>
+            <td><?= $Tugas;?></td>
+            <td><?= $UTS;?></td>
+            <td><?= $UAS;?></td>
+            <td><?= $total_nilai;?></td>
+            <td><?= $keterangan;?></td>        
+        </tr>
+    </table>
+</div>
+</body>
+</html>
---
+++ Praktikum2/proses_nilai.php
@@ -0,0 +1,15 @@
+<?php 
+$nama = $_GET['name'];
+$matkul = $_GET['matkul'];
+$UTS = $_GET['UTS'];
+$UAS = $_GET['UAS'];
+$Tugas = $_GET['Tugas'];
+$total_nilai = ($UTS + $UAS + $Tugas) /3;
+
+if($total_nilai > 75){
+    $keterangan = "LUlUS";
+}else{
+    $keterangan = "TIDAK LULUS";
+}
+
+?>
